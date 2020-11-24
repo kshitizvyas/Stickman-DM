35b66f32ae4e8fc8b499f25753a4f870d1734867
SumeetDevkai <47910905+SumeetDevkai@users.noreply.github.com>
2020-11-24 12:09:09 +0530

    Attack

---
+++ stickman dm/Assets/PlayerMovement.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float HorizontalInputDirection;
+    private float VerticalInputDirection;
+    private float turnTimer;
+
+    private int amountOfJumpsLeft;
+    private int facingDirection = 1;
+
+    private bool isFacingRight = true;
+    private bool isLeft = false;
+    private bool isWalking;
+    private bool isAttacking;
+    private bool isGrounded;
+    private bool canJump;
+    private bool canMove;
+    private bool canFlip;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    public int Health = 5;
+    public int amountOfJumps = 1;
+    [SerializeField]
+    private int GravityScale;
+
+    public float movementSpeed;
+    public float jumpForce;
+    public float groundCheckRadius;
+    public float airDragMultiplier = 0.95f;
+    public float variableJumpHeightMultiplier = 0.5f;
+    public float turnTimerSet = 0.1f;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+
+
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask attackLayers;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
+    }
+
+    void Update()
+    {
+        CheckInput();
+        CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+        CheckSurroundings();
+    }
+
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+    }
+
+    private void CheckIfCanJump()
+    {
+        if ((isGrounded && rb.velocity.y <= 0))
+        {
+            amountOfJumpsLeft = amountOfJumps;
+        }
+
+        if (amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+
+    }
+   
+    private void CheckMovementDirection()
+    {
+        if (isFacingRight && HorizontalInputDirection < 0)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && HorizontalInputDirection > 0)
+        {
+            Flip();
+        }
+
+        if (rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        //anim.SetBool("isWalking", isWalking);
+        //anim.SetBool("isGrounded", isGrounded);
+        //anim.SetFloat("yVelocity", rb.velocity.y);
+        //anim.SetBool("isWallSliding", isWallSliding);
+        anim.SetBool("attack", isAttacking);
+    }
+
+    private void CheckInput()
+    {
+        HorizontalInputDirection = Input.GetAxisRaw("Horizontal");
+        VerticalInputDirection = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            if (!isGrounded && HorizontalInputDirection != facingDirection)
+            {
+                canMove = false;
+                canFlip = false;
+            }
+        }
+
+        
+        if (!canMove)
+        {
+            turnTimer -= Time.deltaTime;
+
+            if (turnTimer <= 0)
+            {
+                canMove = true;
+                canFlip = true;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        isAttacking = true;
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, attackLayers);
+
+        foreach (Collider2D enemy in enemies)
+        {
+            Debug.Log("Attacked enemy : " + enemy.name);
+        }
+    }
+
+    private void Jump()
+    {
+        if (canJump )
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
+    }
+
+    private void ApplyMovement()
+    {
+
+        if (!isGrounded && HorizontalInputDirection == 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x * airDragMultiplier, rb.velocity.y);
+        }
+        else if (canMove)
+        {
+            rb.velocity = new Vector2(movementSpeed * HorizontalInputDirection, rb.velocity.y);
+        }
+
+    }
+
+    private void Flip()
+    {
+        if (canFlip)
+        {
+            facingDirection *= -1;
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+
+    public void Attackbool()
+    {
+        isAttacking = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);   
+    }
+}
---
+++ stickman dm/Assets/Scripts/Photon/RoomButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class RoomButton : MonoBehaviour
+{
+    [SerializeField]
+    private Text nameText;
+    [SerializeField]
+    private Text sizeText;
+
+    private string roomName;
+    private int playerCount;
+    
+    public void JoinRoomOnClick()
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    internal void SetRoom(string nameInput, int sizeInput, int CountInput)
+    {
+        sizeInput = 5;
+
+        roomName = nameInput;
+        playerCount = CountInput;
+        nameText.text = nameInput;
+        sizeText.text = CountInput + "/" + sizeInput;
+    }
+}
